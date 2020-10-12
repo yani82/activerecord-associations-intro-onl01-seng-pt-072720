a13749ba4246728c09e6eb64cc7785d6e0d38b79
yani82 <yani.iman1@gmail.com>
2020-10-12 18:11:05 -0400

    Done.

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,5 +1,5 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
-    def change 
-        add_column :songs, artist_id, :integer
-    end 
+    # def change 
+    #     add_column :songs, :artist_id, :integer
+    # end 
 end
